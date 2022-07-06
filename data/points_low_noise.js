224bc0e85c0091163b8414eb6a8cd32a111a5cb2
Prateek Sahay <ptkinvent@gmail.com>
2022-07-06 01:04:35 +0000

    Added point cloud data

---
+++ data/points_low_noise.js
@@ -0,0 +1,9 @@
+export const lowNoiseVertices = [];
+
+lowNoiseVertices.push(174.1, 178.901, 45.101);
+lowNoiseVertices.push(202.9, 145.701, 27.901);
+lowNoiseVertices.push(203.4, 129.901, 22.401);
+lowNoiseVertices.push(259.4, 131.201, 22.001);
+lowNoiseVertices.push(211.3, 86.101, 12.501);
+lowNoiseVertices.push(237.9, 71.101, 7.301);
+lowNoiseVertices.push(233.1, 4.901, 0.301);
